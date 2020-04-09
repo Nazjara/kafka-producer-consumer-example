366fd9a88d46991ab8c3636768d95e239598304b
Nazar Lipkovskyy <nazar.lipkovskyy@wds.co>
2020-04-09 18:31:02 +0300

    init commit, add Kafka producer

---
+++ src/main/java/com/nazjara/Application.java
@@ -0,0 +1,62 @@
+package com.nazjara;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class Application {
+
+    private static final String TOPIC = "events";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+
+    public static void main(String[] args) {
+        Producer<Long, String> kafkaProducer = createKafkaProducer(BOOTSTRAP_SERVERS);
+
+        try {
+            produceMessages(10, kafkaProducer);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            kafkaProducer.flush();
+            kafkaProducer.close();
+        }
+    }
+
+    public static void produceMessages(long numberOfMessages, Producer<Long, String> kafkaProducer) throws ExecutionException, InterruptedException {
+        int partition = 1;
+
+        for (long i = 0; i < numberOfMessages; i++) {
+            String value = String.format("event %d", i);
+
+            long timeStamp = System.currentTimeMillis();
+
+//            choose partition by hashing key
+//            ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, key, value);
+
+//            choose partition by round robin
+//            ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, value);
+
+//            fixed partition
+            ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, partition, timeStamp, i, value);
+
+            RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+
+            System.out.println(String.format("Record with (key: %s, value: %s), was sent to (partition: %d, offset: %d",
+                    record.key(), record.value(), recordMetadata.partition(), recordMetadata.offset()));
+        }
+    }
+
+    public static Producer<Long, String> createKafkaProducer(String bootstrapServers) {
+        Properties properties = new Properties();
+
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "events-producer");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        return new KafkaProducer<>(properties);
+    }
+}
